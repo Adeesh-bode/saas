01bf3e8aa0ae991d4d184802df7bd2ab8ffe29b6
Adesh Bode <bodead@rknec.edu>
2024-06-20 05:45:57 -0700

    mongoose serverless  connection

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,49 @@
+import mongoose , { Mongoose } from 'mongoose'; // Mongoose for types
+
+const MONGODB_URL = process.env.MONGODB_URL ;
+
+interface MongooseConnection {
+    conn: Mongoose |  null,
+    promise: Promise<Mongoose> | null;
+}
+
+
+let cached : MongooseConnection = ( global as any).mongoose ; // as serveless and new connectn each time caching is a must 
+//cached object to store connection and promise 
+
+if (!cached) { // if not cached earlier
+    cached = (global as any).mongoose = { 
+        conn: null, promise: null 
+    };
+}
+
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn;
+    if (!MONGODB_URL) throw new Error('MONGODB_URL is missing');
+
+    cached.promise = cached.promise ||  mongoose.connect(MONGODB_URL, {
+        dbName: 'imagehelp',
+        bufferCommands:false,
+    });
+
+    cached.conn = await cached.promise;
+
+    return cached.conn;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
